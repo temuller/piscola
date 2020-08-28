d7e754d96f7b589d3d7b9cd7182e240fe491b340
Tomas <t.e.muller-bravo@soton.ac.uk>
2020-08-28 13:21:08 +0100

    test installation, still needs some tweacking

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(
     name="piscola",
     version="1.0",
@@ -13,16 +16,17 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/temuller/piscola",
-    #packages=setuptools.find_packages(),
     packages=['piscola'],
-    package_dir={'': 'src'},
+    package_dir={'piscola': 'src/piscola'},
     python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['matplotlib', 'pandas', 'lmfit', 'peakutils', 'george', 'emcee', 'extinction', 'sfdmap', 'astropy', 'numpy'],
+    install_requires=requirements,
+    #dependency_links=['https://github.com/kbarbary/extinction/tarball/master#egg=extinction'],
     package_data={'piscola' : ["filters/*", "filters/*/*", "templates/*", "templates/*/*", "sfddata-master/*", "README.md", "LICENSE"]},
     include_package_data=True,
+    zip_safe=True
 )
