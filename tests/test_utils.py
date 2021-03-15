7f678ee8e2092caa4abe3382e5eaeddc914abac3
Tomas <t.e.muller-bravo@soton.ac.uk>
2021-03-15 12:18:33 +0000

    update tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,8 +10,8 @@ class TestPiscola(unittest.TestCase):
         
         path = piscola.__path__[0]
         filter_wave, filter_trans = np.loadtxt(os.path.join(path, 'filters/Bessell/Bessell_B.dat')).T
-        
-        for mag_sys in ['Vega', 'BD17']:
+        # VEGA needs to be implemented if necessary in the future
+        for mag_sys in ['BD17']:
             zp = calc_zp(filter_wave, filter_trans, 'photon', mag_sys, 'Bessell_B')
                         
         filter_wave, filter_trans = np.loadtxt(os.path.join(path, 'filters/SDSS/sdss_g.dat')).T
