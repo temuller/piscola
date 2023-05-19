cab456217691986bab81ae5a88303592e742c380
temuller <t.e.muller-bravo@ice.csic.es>
2023-05-19 12:10:20 +0200

    update test with filters

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -5,16 +5,16 @@ import matplotlib.pyplot as plt
 
 class TestPiscola(unittest.TestCase):
     def test_plot(self):
-        filters = MultiFilters(["ps1_g"])
+        filters = MultiFilters(["ps1_g"], ["AB"])
         plt.ion()
         filters.plot_filters()
 
     def test_remove_filter(self):
-        filters = MultiFilters(["ps1_g"])
+        filters = MultiFilters(["ps1_g"], ["AB"])
         filters.remove_filter("ps1_g")
 
     def test_pivot_wavelength(self):
-        filters = MultiFilters(["ps1_g"])
+        filters = MultiFilters(["ps1_g"], ["AB"])
         filters.calc_pivot()
 
 
